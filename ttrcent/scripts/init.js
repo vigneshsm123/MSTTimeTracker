ca38bea3ffd8cc61acea07e51692c3d1f26d4671
Ragul1212 <ragul1212@gmail.com>
2016-12-26 15:09:22 +0530

    Recent
    Recent

--- ttrcent/scripts/init.js
+++ ttrcent/scripts/init.js
@@ -1,17 +1,24 @@
-
-(function() {
-
+(function () {
 	var app = angular.module('myApp', ['ngRoute']);
-app.config(['$routeProvider', function($routeProvider) {
+	app.config(['$routeProvider',function($routeProvider) {
 		$routeProvider.when('/', {
-			templateUrl : "views/login.html",
+			templateUrl : "views/login.html"
 		})
-		.when('/dummy', {
-			templateUrl : "views/dummy.html",
+		
+		.when('/user', {
+			templateUrl : "views/user.html",
+			controller: "userController"
+
+		})
+		.when('/404', {
+			templateUrl : "views/404.html"
 		})
 		.otherwise({
-			redirectTo : "/"
+			redirectTo : "/404"
 		});
-	}]);
- 
+}]);
+app.controller('userController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'LoginController';
+	}); 	
 })();
