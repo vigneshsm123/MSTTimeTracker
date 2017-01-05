cd72882ca533560ce6ac289d90170ed16de1f819
Ragul1212 <ragul1212@gmail.com>
2017-01-05 14:23:53 +0530

    recent-just-now-05
    recent-just-now-05

--- ttrcent/login/controllers/loginController.js
+++ ttrcent/login/controllers/loginController.js
@@ -5,12 +5,13 @@ angular.module('loginModule',[])
    $scope.message='';        
    $scope.login = function(){
           if($scope.username !='' && $scope.password !=''){
-             var res  = loginService.loginAuthentication($scope.username,$scope.password)
-             if(!res){
-                $scope.message='Wrong Username & Passsword';
-                $scope.username = '';
-                $scope.password = '';
-             }
+             loginService.loginAuthentication($scope.username,$scope.password,function(flag){
+                 if(!flag){
+                    $scope.message='Wrong Username & Passsword';
+                    $scope.username = '';
+                    $scope.password = '';
+                 }
+             });
           }                            
           else
          	$scope.message='Please enter username & password';
